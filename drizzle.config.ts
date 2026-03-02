d6b4f99474d90b042dcb7ffed9f96bd60dbf3aec
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2026-03-02 12:19:48 +0000

    Configure drizzle and run first migration.

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { readConfig } from "./src/config";
+
+export default defineConfig({
+  schema: "src/lib/db/schema.ts",
+  out: "src/lib/db/out/",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: readConfig().dbUrl,
+  },
+});
